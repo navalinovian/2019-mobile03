0552ca7599ae8f95cea7365f7f18e5f9a2e4ff09
navalinovian <viansatria57@gmail.com>
2019-09-18 07:19:05 +0700

    Linear Constraint layout Relative Constraint layout

---
+++ ConstraintLayout/app/src/main/java/id/ac/polinema/constraintlayout/LinearConstraint.java
@@ -0,0 +1,14 @@
+package id.ac.polinema.constraintlayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LinearConstraint extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_linear_constraint);
+    }
+}
